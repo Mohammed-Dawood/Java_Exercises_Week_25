7e8f56b4602de61134d04a2ac412493e39a90c08
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-19 19:41:29 +0300

    Exercise_1 done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,7 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Exercise_1.exercise_1();
     }
 }

eec286f7ace666d25a409de5ee4f46eea7d7d7f1
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-19 20:03:10 +0300

    Exercise_2 done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,6 +2,54 @@ package se.lexicon;
 
 public class App {
     public static void main(String[] args) {
-        Exercise_1.exercise_1();
+
+        // To call Exercise_1
+        // Exercise_1.exercise_1();
+
+        // To call Exercise_2
+        Exercise_2.exercise_2();
+
+        // To call Exercise_3
+        // Exercise_3.exercise_3();
+
+        // To call Exercise_4
+        //Exercise_4.exercise_4();
+
+        // To call Exercise_5
+        // Exercise_5.exercise_5();
+
+        // To call Exercise_6
+        // Exercise_6.exercise_6();
+
+        // To call Exercise_7
+        // Exercise_7.exercise_7();
+
+        // To call Exercise_8
+        // Exercise_8.exercise_8();
+
+        // To call Exercise_9
+        // Exercise_9.exercise_9();
+
+        // To call Exercise_10
+        // Exercise_10.exercise_10();
+
+        // To call Exercise_11
+        // Exercise_11.exercise_11();
+
+        // To call Exercise_12
+        // Exercise_12.exercise_12();
+
+        // To call Exercise_13
+        // Exercise_13.exercise_13();
+
+        // To call Exercise_14
+        // Exercise_14.exercise_14();
+
+        // To call Exercise_15
+        // Exercise_15.exercise_15();
+
+        // To call Exercise_16
+        // Exercise_16.exercise_16();
+
     }
 }
